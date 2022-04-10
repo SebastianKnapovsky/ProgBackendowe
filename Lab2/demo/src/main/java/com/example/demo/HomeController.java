2e338f649b918ce5c96f0aa14de5f706cb47e4d8
LeonardoooSanchez <elosknero17@gmail.com>
2022-04-10 12:01:49 +0200

    spring

--- Lab2/demo/src/main/java/com/example/demo/HomeController.java
+++ Lab2/demo/src/main/java/com/example/demo/HomeController.java
@@ -1,17 +1,31 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping("/example")
-
+    @RequestMapping("/")
     @ResponseBody
     public String index() {
         return "Hello World!";
     }
 
+    @RequestMapping("/example")
+    public String example() {
+        return "example";
+    }
+
+    @RequestMapping("/exampleTest")
+    public String exampleTest(@RequestParam(defaultValue = "1") Integer variant) {
+        if (variant == 2 ) {
+            return "example2";
+        }
+
+        return "example";
+    }
 }
