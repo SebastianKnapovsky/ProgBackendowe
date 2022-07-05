778008babbb87eadcd305d5415943412fd52cc0a
Sebastian Knap <sebastian.knap@oltis.pl>
2022-07-05 13:44:02 +0200

    Implementacja EF + utworzenie bazy danych i tabeli

---
+++ Projekt/ProjZal/RestaurantAPI/RestaurantAPI/Entities/RestaurantDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RestaurantAPI.Entities
+{
+    public class RestaurantDbContext : DbContext 
+    {
+        private string _connectionString = "Data Source=SKNAP;User ID=wp_user;Password=corridor;Initial Catalog=RestaurantDb;MultipleActiveResultSets=true";
+        public DbSet<Restaurant> Restaurants { get; set; }
+        public DbSet<Adress> Adresses { get; set; }
+        public DbSet<Dish> Dishes { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Restaurant>()
+                .Property(r => r.Name)
+                .IsRequired()
+                .HasMaxLength(25);
+            modelBuilder.Entity<Dish>()
+                .Property(d => d.Name)
+                .IsRequired();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+    }
+}
