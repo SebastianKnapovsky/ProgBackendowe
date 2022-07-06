aca8861938ac19b9cf0af6dfd094de6127fa84f6
Sebastian Knap <sebastian.knap@oltis.pl>
2022-07-06 11:31:08 +0200

    Dodanie Middleware

--- Projekt/ProjZal/RestaurantAPI/RestaurantAPI/Program.cs
+++ Projekt/ProjZal/RestaurantAPI/RestaurantAPI/Program.cs
@@ -1,6 +1,7 @@
 using NLog.Web;
 using RestaurantAPI;
 using RestaurantAPI.Entities;
+using RestaurantAPI.Middleware;
 using RestaurantAPI.Services;
 using System.Reflection;
 
@@ -16,6 +17,7 @@ builder.Services.AddDbContext<RestaurantDbContext>();
 builder.Services.AddScoped<RestaurantSeeder>();
 builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 builder.Services.AddScoped<IRestaurantService, RestaurantService>();
+builder.Services.AddScoped<ErrorHandlingMiddleware>();
 
 var app = builder.Build();
 
@@ -23,6 +25,7 @@ var scope = app.Services.CreateScope();
 var seeder = scope.ServiceProvider.GetRequiredService<RestaurantSeeder>();
 // Configure the HTTP request pipeline.
 seeder.Seed();
+app.UseMiddleware<ErrorHandlingMiddleware>();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
