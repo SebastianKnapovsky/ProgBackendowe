6e6a3ccd42f808898a89ecdf766d5a192fcca799
Sebastian Knap <sebastian.knap@oltis.pl>
2022-07-06 15:03:11 +0200

    Pobieranie dań wszystkich i pojedyńczych po Id

--- Projekt/ProjZal/RestaurantAPI/RestaurantAPI/Services/DishService.cs
+++ Projekt/ProjZal/RestaurantAPI/RestaurantAPI/Services/DishService.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
 using RestaurantAPI.Entities;
 using RestaurantAPI.Exceptions;
 using RestaurantAPI.Models;
@@ -18,7 +19,7 @@ namespace RestaurantAPI.Services
         public int Create(int restaurantId, CreateDishDto dto)
         {
             var restaurant = _context.Restaurants.FirstOrDefault(r => r.Id == restaurantId);
-            if (restaurant == null)
+            if (restaurant is null)
                 throw new NotFoundException("Restaurant not found");
 
             var dishEntity = _mapper.Map<Dish>(dto);
@@ -29,6 +30,35 @@ namespace RestaurantAPI.Services
             _context.SaveChanges();
 
             return dishEntity.Id;
+        }
+        public DishDto GetById(int restaurantId, int dishId)
+        {
+            var restaurant = _context.Restaurants.FirstOrDefault(r => r.Id == restaurantId);
+            if (restaurant is null)
+                throw new NotFoundException("Restaurant not found");
+
+            var dish = _context.Dishes.FirstOrDefault(d => d.Id == dishId);
+            if(dish is null || dish.RestaurantId != restaurantId)
+            {
+                throw new NotFoundException("Dish not found");
+            }
+            var dishDto = _mapper.Map<DishDto>(dish);
+
+            return dishDto;
+        }
+
+        public List<DishDto> GetAll(int restaurantId)
+        {
+            var restaurant = _context
+                .Restaurants
+                .Include(r => r.Dishes)
+                .FirstOrDefault(r => r.Id == restaurantId);
+            if (restaurant is null)
+                throw new NotFoundException("Restaurant not found");
+
+            var dishDtos = _mapper.Map<List<DishDto>>(restaurant.Dishes);
+
+            return dishDtos;
         }
     }
 }
